8e0cd44de829c5c21695e2aa54469bb791da328d
DavidG <david.gl@logicalcommander.com>
2025-05-05 13:59:34 -0300

    fix: adjusting margin for prayer generation button in PrayerSheet component

--- components/PrayerSheet.tsx
+++ components/PrayerSheet.tsx
@@ -226,7 +226,7 @@ const PrayerSheet: React.FC<PrayerSheetProps> = ({
           </View>
 
           {/* Generate button */}
-          <View className="mt-48">
+          <View className="mt-28">
             <PrimaryButton
               title="Generate a prayer"
               onPress={handlePrayerGenerate}
