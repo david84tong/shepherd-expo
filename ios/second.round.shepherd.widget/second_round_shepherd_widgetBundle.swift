fc5c2540a5715f8c4f49840bf9271ceda6b8cd6c
Dante <motiv8lab@gmail.com>
2025-05-21 20:29:04 -0700

    fds

---
+++ ios/second.round.shepherd.widget/second_round_shepherd_widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  second_round_shepherd_widgetBundle.swift
+//  second.round.shepherd.widget
+//
+//  Created by Dante Kim on 5/21/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct second_round_shepherd_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        second_round_shepherd_widget()
+        second_round_shepherd_widgetControl()
+        second_round_shepherd_widgetLiveActivity()
+    }
+}
