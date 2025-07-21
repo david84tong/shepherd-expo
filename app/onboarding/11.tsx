d2d0b6bdacf194fbb7e6b06c0bdd29c0708151fa
Abdul Manan <sweetrajput181@gmail.com>
2025-07-21 17:45:31 +0500

    fix: added login and sign up by email and password for ios

--- app/onboarding/11.tsx
+++ app/onboarding/11.tsx
@@ -976,7 +976,7 @@ export default function SaveProgressScreen() {
           )}
 
           {/* Email/Password Form - Always Visible */}
-          {showEmailPassword && IS_ANDROID && (
+          {showEmailPassword  && (
             <Animated.View style={buttonsStyle} className="mb-6">
               <View className="w-full mb-4">
                 <TextInput
@@ -1047,7 +1047,7 @@ export default function SaveProgressScreen() {
               <Animated.View style={buttonsStyle}>
                 {isSmaleAge && Platform.OS === 'android' ? null : (
                   <View className="items-center mb-4">
-                    {Platform.OS === 'ios' ? (
+                    {Platform.OS === 'ios' ? showEmailPassword?null: (
                       <TouchableOpacity
                         style={{ height: RPH(6) }}
                         className="flex-row items-center justify-center bg-black w-full  px-6 rounded-[16px] mb-4 shadow-appleShadow"
